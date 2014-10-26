8211fa5d4b1c66b73d8032d582dc7120f2b44e83
Cody Boppert <cody@eyeviewdigital.com>
2014-10-25 23:45:06 -0400

    Generate parts

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
+import java.util.HashMap;
+import java.util.Random;
+
 /**
  * Created by codyboppert on 10/25/14.
  */
 public class Main {
     public static void main(String[] args) {
         //Initialize
-            //Generate tiles
+        //Generate parts
+        HashMap<Part, Integer> parts = new HashMap<Part, Integer>();
+        Part[] partArray = new Part[10];
+        int index = 0;
+        for (Color color : Color.values()) {
+            Part top = new Part(true, color);
+            partArray[index] = top;
+            parts.put(top, 10);
+            index++;
 
-            //Generate boards
+            Part bottom = new Part(false, color);
+            partArray[index] = bottom;
+            parts.put(bottom, 10);
+            index++;
+        }
+        //Generate tiles
+        Random random = new Random(System.currentTimeMillis());
+
+        //Generate boards
 
 
 
